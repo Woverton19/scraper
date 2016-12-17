36da79d3fc382efdd34a33b3ff23fdd5bd65f75e
John Overton <John Overton>
2016-12-17 17:33:14 -0600

    first push not working it deletes whole article

---
+++ models/Articles.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  },
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// Create the Article model with the ArticleSchema
+var Articles = mongoose.model("Articles", ArticleSchema);
+
+// Export the model
+module.exports = Articles;
